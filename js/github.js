d813489a6d243b9c9189af068cc2ee6c7b02fe5f
hrudai2002 <hrudai2002@gmail.com>
2021-06-18 10:21:14 +0530

    files added

---
+++ js/github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+        this.client_id = 'c5d82f47f6126783bfc0';
+        this.client_secret = '935f1342ffb559df17c94982a4e55f195c2c6870';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
